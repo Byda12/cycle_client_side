5f60eebc646da16c24a425620d3f2b1212f5ff32
Byda12 <bydaeducationcenter@gmail.com>
2024-10-01 14:05:06 +0600

    first commit

---
+++ src/Pages/Login/Login/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Login.css';
+
+
+const Login = () => {
+    return (
+        <div>
+            <h2>I'm From Login Page</h2>
+            
+        </div>
+    );
+};
+
+export default Login;
