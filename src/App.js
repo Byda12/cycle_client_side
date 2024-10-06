806f876f977f2fa1fdb3fbe48fe3526230553cfe
Byda12 <bydaeducationcenter@gmail.com>
2024-10-06 11:48:52 +0600

    react router dom addded

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import Home from './Pages/Home/Home/Home';
+import Header from './Pages/Shared/Header/Header';
+import Footer from './Pages/Shared/Footer/Footer';
+import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
+import NotFound from './Pages/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
+      <Login></Login>
       <Home></Home>
-        
+      <Register></Register>
+      <NotFound></NotFound>
+     <Footer></Footer>
     </div>
   );
 }
 
 export default App;
+
